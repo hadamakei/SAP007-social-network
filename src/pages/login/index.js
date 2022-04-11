8257a52fcf8daa4c81e3bc5c65d52da24e67f4ac
Tatiane Mendonça <tatimendonz@gmail.com>
2022-04-11 18:47:34 -0300

    gmail esqueci senha funcionando

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,5 @@
 import {
-  auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup,
+  auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, getRedirectResult,
 } from '../../lib/authfirebase.js';
 
 export default () => {
@@ -95,13 +95,16 @@ export default () => {
     }
   };
 
+  // AUTENTICAÇÃO VIA GOOGLE
+
   const provider = new GoogleAuthProvider();
   function loginGoogle() {
     signInWithPopup(auth, provider)
       .then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
+        console.log(credential);
+        // const token = credential.accessToken;
         // The signed-in user info.
         const user = result.user;
         window.location.href = '#feed';
@@ -112,13 +115,40 @@ export default () => {
       // Handle Errors here.
         const errorCode = error.code;
         const errorMessage = error.message;
-        // The email of the user's account used.
+        console.log(errorCode);
+        console.log(errorMessage);
         const email = error.email;
         // The AuthCredential type that was used.
         const credential = GoogleAuthProvider.credentialFromError(error);
         // ...
       });
   }
+
+  // RECUPERAR SENHA DO GOOGLE
+
+  getRedirectResult(auth) // precisa ser resolvido antes do acionamento
+    .then((result) => {
+      // Isso lhe dá um token de acesso do Google. Você pode usá-lo para acessar as APIs do Google.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+
+      // As informações do usuário conectado.
+      const user = result.user;
+      console.log(user);
+    }).catch((error) => {
+      // Tratar os Errors aqui.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // console.log(errorCode);
+      // console.log(errorMessage);
+
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+
   container.querySelector('#bt-google').addEventListener('click', loginGoogle);
 
   container.querySelector('#button').addEventListener('click', loginEmailPassword);
