b9b0dae44d0da19d37027710847ea262bf1b9d07
Larissa Moreira <larissamoreiracarvalho@gmail.com>
2022-04-18 23:48:12 -0300

    pagina home iniciada

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,12 @@
 
 export default () => {
   const container = document.createElement('div');
-
+  container.classList.add('home-container');
 
   const template = `
   <div class="logo-container">
-    <img src="/pages/style/logocores.png">
-    <img src="/pages/style/escrita.png">
+    <img class="logo" src="/pages/style/logocores.png">
+    <img class="escrita" src="/pages/style/escrita.png">
   </div>
     <button class="botao-home" id="button">Entrar</button>
     `;
