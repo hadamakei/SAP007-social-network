71dd8b551ebcfa4d1e5f5b8bd9de8e9475a179ef
Keila Hadama <hadamakeila@gmail.com>
2022-04-25 19:46:01 -0300

    inicio refatorando

--- src/lib/firestore.js
+++ src/lib/firestore.js
@@ -1,5 +1,5 @@
 import {
-  getFirestore, collection, addDoc, getDocs, deleteDoc, doc, onSnapshot, query, where, updateDoc, orderBy, serverTimestamp, Timestamp, limit,
+    getFirestore, collection, addDoc, getDocs, deleteDoc, doc, onSnapshot, query, where, updateDoc, orderBy, serverTimestamp, Timestamp, limit,
 } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js';
 import { firebaseApp } from '../../lib/serverfirebase.js';
 
@@ -19,6 +19,38 @@ const dataBase = getFirestore(firebaseApp);
 //     return arrPosts;
 //   };
 
+const collectionName = collection(dataBase, 'posts');
+
+// // //Queries traz todos posts de todos usuarios
+const queryAllPosts = query(collectionName, orderBy('data', 'desc'), limit(10));
+
+export async function readDocument() {
+    return await getDocs(queryAllPosts)
+    
+}
+
+
+export async function addDocPosts(date, addPost, user) {
+   return await addDoc(collectionName, {
+        data: date,
+        mensagem: addPost.value,
+        user: {
+            userId: user.uid,
+            photUrl: user.photoURL,
+        },
+    })
+}
+
+// export async function updateDocPost(collectionUpdate, newText, dateFormat){
+    
+//     updateDoc(collectionUpdate, {
+//         mensagem: newText,
+//         data: dateFormat,
+//       });
+// }
+
+
+
 export {
-  dataBase, collection, addDoc, getDocs, deleteDoc, doc, onSnapshot, query, where, updateDoc, orderBy, serverTimestamp, Timestamp, limit,
+    dataBase, collection, addDoc, getDocs, deleteDoc, doc, onSnapshot, query, where, updateDoc, orderBy, serverTimestamp, Timestamp, limit,
 };
