feef3f74c327d4dac8cd30430165775ddc723185
Tatiane Mendonça <tatimendonz@gmail.com>
2022-04-30 04:03:11 -0300

    quase pronto

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -36,12 +36,11 @@ export default () => {
       <div class="logo-container">
       <img class="logo" src="/pages/style/logo.png">
       </div>
-      <div class="form">
-      <div class="error" id="email-required-error">Email é obrigatório</div>
-      <div class="error" id="email-invalid-error">Ops, email inválido</div>
-      <div class="error" id="password-required-error">Ops, senha inválida</div>
-      <div class="error" id="user-invalid-error">Usuário inválido!</div>
-      <div></div>
+        <div class="form">
+        <div class="error" id="email-required-error">Email é obrigatório</div>
+        <div class="error" id="email-invalid-error">Ops, email inválido</div>
+        <div class="error" id="password-required-error">Ops, senha inválida</div>
+        <div class="error" id="user-invalid-error">Usuário inválido!</div>
         <input class="control" type="text" placeholder="Usúario" id="name" required></input>
         <input class="control" type="email" placeholder="Email" id="email" required></input>
         <input class="control" type="password" placeholder="Senha" id="password" required></input>
@@ -54,59 +53,6 @@ export default () => {
 
   container.innerHTML = template;
 
-  // validados do email
-  function validateEmail(email) {
-    return /\S+@\S+\.\S+/.test(email);
-  }
-  function isEmailValid() {
-    const email = document.getElementById('email').value;
-    if (!email) {
-      return false;
-    }
-    return validateEmail(email);
-  }
-
-  // function isPasswordValid() {
-  //   const password = document.getElementById('password').value;
-  //   if (!password) {
-  //     return false;
-  //   }
-  //   return true;
-  // }
-
-  // validador de usuário
-  function validateUser(user) {
-    return /^[a-zA-Z0-9_]+$/.test(user);
-  }
-  function isUserValid() {
-    const user = document.getElementById('name').value;
-    if (!user) {
-      return false;
-    }
-    return validateUser(user);
-  }
-
-  function onchangeEmail() {
-    buttonsDisable();
-    emailErrors();
-  }
-
-  function onchangePassword() {
-    buttonsDisable();
-    passawordErrors();
-  }
-  function onchangeUser() {
-    buttonsDisable();
-    userErrors();
-  }
-
-  // // validador do botão Cadastre-se
-  // function validate() {
-  //   const emailValid = isEmailValid();
-  //   const userValid = isUserValid();
-  //   document.getElementById('bt-register').disabled = !emailValid || !userValid;
-  // }
-
   // AUTENTICAÇÃO VIA GOOGLE
   const provider = new GoogleAuthProvider();
   function loginGoogle() {
@@ -118,6 +64,42 @@ export default () => {
       .catch((error) => {
         alert(getErrorMessage(error));
       });
+  }
+
+  const form = {
+    registerButton: () => document.getElementById('bt-register'),
+    email: () => document.getElementById('email'),
+    emailInvalidError: () => document.getElementById('email-invalid-error'),
+    emailRequiredError: () => document.getElementById('email-required-error'),
+    password: () => document.getElementById('password'),
+    userInvalidError: () => document.getElementById('user-invalid-error'),
+    passwordRequiredError: () => document.getElementById('password-required-error'),
+    user: () => document.getElementById('name'),
+
+  };
+
+  // validador do email
+  function validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+  function isEmailValid() {
+    const email = form.email().value;
+    if (!email) {
+      return false;
+    }
+    return validateEmail(email);
+  }
+
+  // validador de usuário
+  function validateUser(user) {
+    return /^[a-zA-Z0-9_]+$/.test(user);
+  }
+  function isUserValid() {
+    const user = form.user().value;
+    if (!user) {
+      return false;
+    }
+    return validateUser(user);
   }
 
   // Mensagens de erro
@@ -150,50 +132,62 @@ export default () => {
 
   // // mensagens de erro UserName
   function userErrors() {
-    const userName = document.getElementById('name').value;
-    console.log(userName);
+    const userName = form.user().value;
     if (!userName) {
-      document.getElementById('user-invalid-error').style.display = 'block';
+      form.userInvalidError().style.display = 'block';
     } else {
-      document.getElementById('user-invalid-error').style.display = 'none';
+      form.userInvalidError().style.display = 'none';
     }
   }
 
   // mensagens de erro Email
   function emailErrors() {
-    const email = document.getElementById('email').value;
+    const email = form.email().value;
     if (!email) {
-      document.getElementById('email-required-error').style.display = 'block';
+      form.emailRequiredError().style.display = 'block';
     } else {
-      document.getElementById('email-required-error').style.display = 'none';
+      form.emailRequiredError().style.display = 'none';
     }
     if (validateEmail(email)) {
-      document.getElementById('email-invalid-error').style.display = 'none';
+      form.emailInvalidError().style.display = 'none';
     } else {
-      document.getElementById('email-invalid-error').style.display = 'block';
+      form.emailInvalidError().style.display = 'block';
     }
   }
 
   // mensagens de erro Password
   function passawordErrors() {
-    const password = document.getElementById('password').value;
+    const password = form.password().value;
     if (!password) {
-      document.getElementById('password-required-error').style.display = 'block';
+      form.passwordRequiredError().style.display = 'block';
     } else {
-      document.getElementById('password-required-error').style.display = 'none';
+      form.passwordRequiredError().style.display = 'none';
     }
   }
 
   function buttonsDisable() {
     const emailValid = isEmailValid();
     const userValid = isUserValid();
-    document.getElementById('bt-register').disabled = !emailValid || !userValid;
+    form.registerButton().disabled = !emailValid || !userValid;
+  }
+
+  function onchangeEmail() {
+    buttonsDisable();
+    emailErrors();
+  }
+
+  function onchangePassword() {
+    buttonsDisable();
+    passawordErrors();
+  }
+  function onchangeUser() {
+    buttonsDisable();
+    userErrors();
   }
 
   container.querySelector('#password').addEventListener('change', onchangePassword);
   container.querySelector('#email').addEventListener('change', onchangeEmail);
   container.querySelector('#name').addEventListener('change', onchangeUser);
-
   container.querySelector('#bt-google').addEventListener('click', loginGoogle);
   container.querySelector('#bt-register').addEventListener('click', createAccount);
 
