fb9d3bd151dbc17cd6abca1429101f9b8cd22b5d
Keila Hadama <hadamakeila@gmail.com>
2022-05-07 16:08:32 -0300

    inicio testes

--- test/firestore.spec.js
+++ test/firestore.spec.js
@@ -3,7 +3,7 @@ import { addDocPosts } from '../src/lib/firestore';
 jest.mock('../src/lib/exports.js');
 
 describe('addDocPosts', () => {
-  it('deveria chamar a funcao addDoc do firebase', () => {
+  it('deveria chamar a funcao addDoc do firebase!', () => {
     expect(typeof addDocPosts).toBe('function');
   });
 });
