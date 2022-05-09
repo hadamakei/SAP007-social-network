fb0c5bea3118e67122ad31ae1466a561b4018682
Keila Hadama <hadamakeila@gmail.com>
2022-05-09 14:18:09 -0300

    ajustes eslint

--- test/authfirebase.spec.js
+++ test/authfirebase.spec.js
@@ -1,4 +1,4 @@
-/* 
+/*
 * @jest-enviroment jsdom
 */
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from '../src/lib/authfirebase';
@@ -19,38 +19,36 @@ it('loginEmailPassword should fail to login user and return error code', async (
   jest.clearAllMocks();
   exportsFunctions.signInWithEmailAndPassword.mockRejectedValue({
     code: 'Fail to login user',
-  })
+  });
   try {
     await signInWithEmailAndPassword('n77CRLrfjbMTvIZgkGs3ckqj7HN2', 'teste1@teste.com', '654321');
   } catch (error) {
     expect(exportsFunctions.signInWithEmailAndPassword).toHaveBeenCalledTimes(1);
     expect(error.code).toEqual('Fail to login user');
-    //verificar se é chamado com parametros certos
+    // verificar se é chamado com parametros certos
   }
 });
 
 it('createAccount should create user with auth, email and password', async () => {
   exportsFunctions.createUserWithEmailAndPassword.mockResolvedValue({
     user: {},
-  })
-  const user = await createUserWithEmailAndPassword('n77CRLrfjbMTvIZgkGs3ckqj7HN2', 'teste@teste.com', '123456');
+  });
+  await createUserWithEmailAndPassword('n77CRLrfjbMTvIZgkGs3ckqj7HN2', 'teste@teste.com', '123456');
   expect(exportsFunctions.createUserWithEmailAndPassword).toHaveBeenCalledTimes(1);
   expect(exportsFunctions.createUserWithEmailAndPassword).toHaveBeenCalledWith('n77CRLrfjbMTvIZgkGs3ckqj7HN2', 'teste@teste.com', '123456');
-  //verificar se user criado é igual o q mockou acima
+  // verificar se user criado é igual o q mockou acima
 });
 
 it('createAccount should fail to create user and return error code', async () => {
   jest.clearAllMocks();
   exportsFunctions.createUserWithEmailAndPassword.mockRejectedValue({
     code: 'Fail to create user',
-  })
+  });
   try {
     await createUserWithEmailAndPassword('n77CRLrfjbMTvIZgkGs3ckqj7HN2', 'teste@teste.com', '123456');
   } catch (error) {
     expect(exportsFunctions.createUserWithEmailAndPassword).toHaveBeenCalledTimes(1);
     expect(error.code).toEqual('Fail to create user');
-    //verificar se é chamado com parametros certos
+    // verificar se é chamado com parametros certos
   }
 });
-
-
--- test/firestore.spec.js
+++ test/firestore.spec.js
@@ -5,16 +5,16 @@ jest.mock('../src/lib/exports.js');
 
 it('addDocPosts should add posts with date, message, user name, id, photoUrl, list of likes', async () => {
   exportsFunctions.addDoc.mockResolvedValue({
-      data: '07/05/2022 16:00:42',
-      mensagem: 'texto',
-      user: {
-        name: 'nome',
-        userId: 'n77CRLrfjbMTvIZgkGs3ckqj7HN2',
-        photUrl: '',
-      },
-      listaLikes: [],
-  })
-  await addDoc('07/05/2022 16:00:42', 'texto', 'nome','n77CRLrfjbMTvIZgkGs3ckqj7HN2', '', []);
+    data: '07/05/2022 16:00:42',
+    mensagem: 'texto',
+    user: {
+      name: 'nome',
+      userId: 'n77CRLrfjbMTvIZgkGs3ckqj7HN2',
+      photUrl: '',
+    },
+    listaLikes: [],
+  });
+  await addDoc('07/05/2022 16:00:42', 'texto', 'nome', 'n77CRLrfjbMTvIZgkGs3ckqj7HN2', '', []);
   expect(exportsFunctions.addDoc).toHaveBeenCalledTimes(1);
 });
 
@@ -22,12 +22,12 @@ it('addDocPosts should fail to add user post and return error code', async () =>
   jest.clearAllMocks();
   exportsFunctions.addDoc.mockRejectedValue({
     code: 'Fail to add user post',
-  })
+  });
   try {
-    await addDoc('07/05/2022 16:00:42', 'texto', 'nome','n77CRLrfjbMTvIZgkGs3ckqj7HN2', '', []);
+    await addDoc('07/05/2022 16:00:42', 'texto', 'nome', 'n77CRLrfjbMTvIZgkGs3ckqj7HN2', '', []);
   } catch (error) {
     expect(exportsFunctions.addDoc).toHaveBeenCalledTimes(1);
     expect(error.code).toEqual('Fail to add user post');
-    //verificar se é chamado com parametros certos
+    // verificar se é chamado com parametros certos
   }
 });
